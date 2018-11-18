094284f95107977ae5fbf079dee6ce4a732e1359
Snehasis <snehasis.c@lucideustech.com>
2018-11-18 09:19:51 +0530

    done the code for managing animals life

--- perform_game.php
+++ perform_game.php
@@ -1,19 +1,98 @@
 <?php
 session_start();
-$final_result_array = isset($_SESSION['final_result_array']) ? $_SESSION['final_result_array'] : [];
-$no_of_times_button_clicked = $_POST['button_clicked'];
-$animal_person_array = array('Farmer','Cow1','Cow2','Bunny1','Bunny2','Bunny3','Bunny4');
 
-$a = $animal_person_array[array_rand($animal_person_array,1)];
+if(isset($_SESSION['animals']) && isset($_SESSION['animal_life']) && isset($_SESSION['final_result']))
+{
+	perform_game_logic();
+}
+else
+{
+	$_SESSION['animals'] = array("Farmer","Cow1","Cow2","Bunny1","Bunny2","Bunny3","Bunny4");
+	$_SESSION['animal_life'] = array('F'=>15,'C1'=>10,'C2'=>10,'B1'=>8,'B2'=>8,'B3'=>8,'B4'=>8);
+	perform_game_logic();
+}
 
-$final_result_array[] = array($no_of_times_button_clicked => $a);
+function perform_game_logic()
+{
+	$no_of_times_button_clicked = $_POST['button_clicked'];
+	$random_animal = $_SESSION['animals'][array_rand($_SESSION['animals'],1)];
+	$_SESSION['final_result'][] = array($no_of_times_button_clicked => $random_animal);
 
-  
-  $_SESSION['final_result_array'] = $final_result_array;
-//print_r($final_result_array);
-  //echo json_encode($final_result_array);
-//unset($_SESSION['final_result_array']);
+	switch ($random_animal) {
+		case "Farmer":
+			$_SESSION['animal_life']['F'] = 15;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Cow1":
+			$_SESSION['animal_life']['C1'] = 10;
+			$_SESSION['animal_life']['F'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Cow2":
+			$_SESSION['animal_life']['C2'] = 10;
+			$_SESSION['animal_life']['F'] -= 1;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Bunny1":
+			$_SESSION['animal_life']['B1'] = 8;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['F'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Bunny2":
+			$_SESSION['animal_life']['B2'] = 8;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['F'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Bunny3":
+			$_SESSION['animal_life']['B3'] = 8;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['F'] -= 1;
+			$_SESSION['animal_life']['B4'] -= 1;
+			break;
+		case "Bunny4":
+			$_SESSION['animal_life']['B4'] = 8;
+			$_SESSION['animal_life']['C1'] -= 1;
+			$_SESSION['animal_life']['C2'] -= 1;
+			$_SESSION['animal_life']['B1'] -= 1;
+			$_SESSION['animal_life']['B2'] -= 1;
+			$_SESSION['animal_life']['B3'] -= 1;
+			$_SESSION['animal_life']['F'] -= 1;
+			break;
+	} 
+	
+	
 
+	//print_r($_SESSION['animal_life']);
+}
+//echo $random_animal;
+ //print_r($_SESSION['animal_life']);
+//  unset($_SESSION['animals']);
+//  unset($_SESSION['animal_life']);
+// unset($_SESSION['final_result']);
 
 
 
